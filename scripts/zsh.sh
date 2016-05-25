a69550ad6d2dc6a994b053538eba6af37ac0710f
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2016-05-25 17:04:19 +0200

    Add zsh

---
+++ scripts/zsh.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sudo su vagrant
+echo ">>> Install Oh-My-ZSH"
+sudo apt-get install -y zsh
+wget http://install.ohmyz.sh -O - | zsh
+
+echo ">>> Install powerlevel9k theme"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k\/powerlevel9k"/' ~/.zshrc
+
+echo ">>> Install Z"
+git clone https://github.com/rupa/z.git
+mv z/z.sh ~/.z.sh
+touch ~/.z
+rm -rf z
+
+echo ">>> Activate Z and update powerlevel9k config"
+cat >> ~/.zshrc << EOF
+# Enable Z
+. ~/.z.sh
+# POWERLEVEL9K SETTINGS #
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(load ram)
+POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+EOF
+
+sudo chsh -s /bin/zsh
