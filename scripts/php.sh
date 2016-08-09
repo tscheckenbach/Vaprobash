e679aeb5ae2d6798416098560cedbbb7cdbf5703
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2016-08-09 23:10:57 +0200

    Update php provisioning
    Support version 5.6 and 7.0

--- scripts/php.sh
+++ scripts/php.sh
@@ -31,42 +31,29 @@ if [[ $HHVM == "true" ]]; then
 else
     echo ">>> Installing PHP $PHP_VERSION"
 
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-
-    if [ $PHP_VERSION == "5.5" ]; then
-        # Add repo for PHP 5.5
-        sudo add-apt-repository -y ppa:ondrej/php5
-    else
-        # Add repo for PHP 5.6
-        sudo add-apt-repository -y ppa:ondrej/php5-5.6
-    fi
-
-    sudo apt-key update
-    sudo apt-get update
-
     # Install PHP
     # -qq implies -y --force-yes
-    sudo apt-get install -qq php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-memcached php5-imagick php5-intl php5-xdebug
-
+    sudo apt-get install -qq php$PHP_VERSION-cli php$PHP_VERSION-fpm
+    sudo apt-get install -qq php-mysql php-pgsql php-curl php-gd php-gmp php-mcrypt php-sqlite3 php-memcached php-imagick php-intl php-xdebug
     # Set PHP FPM to listen on TCP instead of Socket
-    sudo sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
 
     # Set PHP FPM allowed clients IP address
-    sudo sed -i "s/;listen.allowed_clients/listen.allowed_clients/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/;listen.allowed_clients/listen.allowed_clients/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
 
-    # Set run-as user for PHP5-FPM processes to user/group "vagrant"
+    # Set run-as user for php$PHP_VERSION-FPM processes to user/group "ubuntu"
     # to avoid permission errors from apps writing to files
-    sudo sed -i "s/user = www-data/user = vagrant/" /etc/php5/fpm/pool.d/www.conf
-    sudo sed -i "s/group = www-data/group = vagrant/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/user = www-data/user = ubuntu/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
+    sudo sed -i "s/group = www-data/group = ubuntu/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
 
-    sudo sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php5/fpm/pool.d/www.conf
-    sudo sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php5/fpm/pool.d/www.conf
-    sudo sed -i "s/listen\.mode.*/listen.mode = 0666/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/listen\.owner.*/listen.owner = ubuntu/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
+    sudo sed -i "s/listen\.group.*/listen.group = ubuntu/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
+    sudo sed -i "s/listen\.mode.*/listen.mode = 0666/" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
 
 
     # xdebug Config
-    cat > $(find /etc/php5 -name xdebug.ini) << EOF
-zend_extension=$(find /usr/lib/php5 -name xdebug.so)
+    cat > $(find /etc/php/$PHP_VERSION -name xdebug.ini) << EOF
+zend_extension=xdebug.so
 xdebug.remote_enable = 1
 xdebug.remote_connect_back = 1
 xdebug.remote_port = 9000
@@ -81,12 +68,12 @@ xdebug.var_display_max_data = 1024
 EOF
 
     # PHP Error Reporting Config
-    sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-    sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+    sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$PHP_VERSION/fpm/php.ini
+    sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$PHP_VERSION/fpm/php.ini
 
     # PHP Date Timezone
-    sudo sed -i "s/;date.timezone =.*/date.timezone = ${PHP_TIMEZONE/\//\\/}/" /etc/php5/fpm/php.ini
-    sudo sed -i "s/;date.timezone =.*/date.timezone = ${PHP_TIMEZONE/\//\\/}/" /etc/php5/cli/php.ini
+    sudo sed -i "s/;date.timezone =.*/date.timezone = ${PHP_TIMEZONE/\//\\/}/" /etc/php/$PHP_VERSION/fpm/php.ini
+    sudo sed -i "s/;date.timezone =.*/date.timezone = ${PHP_TIMEZONE/\//\\/}/" /etc/php/$PHP_VERSION/cli/php.ini
 
-    sudo service php5-fpm restart
+    sudo service php$PHP_VERSION-fpm restart
 fi
