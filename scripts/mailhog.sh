378c3dedc4a503a63f81f2715cbb7865a6c2cdcb
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2016-08-22 14:21:13 +0200

    Update mailhog provisioning add php5 & 7 option

--- scripts/mailhog.sh
+++ scripts/mailhog.sh
@@ -25,7 +25,8 @@ sudo service mailhog start
 echo ">>> Installing mhsendmail"
 source /home/ubuntu/.gvm/scripts/gvm
 go get github.com/mailhog/mhsendmail
-echo "sendmail_path = $GOPATH/bin/mhsendmail" | sudo tee /etc/php5/mods-available/mailhog.ini
-sudo php5enmod mailhog
+PHP_VERSION=$(find /etc/php -mindepth 1 -maxdepth 1 -type d | grep -o "[[:digit:]]\.[[:digit:]]")
+echo "sendmail_path = $GOPATH/bin/mhsendmail" | sudo tee /etc/php$PHP_VERSION/mods-available/mailhog.ini
+sudo php$PHP_VERSIONenmod mailhog
 
-sudo service php5-fpm restart
+sudo service php$PHP_VERSION-fpm restart
