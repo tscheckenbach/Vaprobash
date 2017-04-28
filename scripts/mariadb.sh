b84d01047711674b15dac4ffac45b52e4b99df9d
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2017-04-28 14:44:14 +0200

    Update mariaDB install

--- scripts/mariadb.sh
+++ scripts/mariadb.sh
@@ -4,23 +4,6 @@ echo ">>> Installing MariaDB"
 
 [[ -z $1 ]] && { echo "!!! MariaDB root password not set. Check the Vagrant file."; exit 1; }
 
-# default version
-MARIADB_VERSION='10.1'
-
-# Import repo key
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-
-# Add repo for MariaDB
-sudo add-apt-repository "deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/$MARIADB_VERSION/ubuntu trusty main"
-
-# Update
-sudo apt-get update
-
-# Install MariaDB without password prompt
-# Set username to 'root' and password to 'mariadb_root_password' (see Vagrantfile)
-sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password password $1"
-sudo debconf-set-selections <<< "maria-db-$MARIADB_VERSION mysql-server/root_password_again password $1"
-
 # Install MariaDB
 # -qq implies -y --force-yes
 sudo apt-get install -qq mariadb-server
