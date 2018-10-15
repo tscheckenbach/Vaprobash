ae2af45c3c12c95b99b9f187b1c9d31d32d82669
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2018-10-15 15:39:23 +0200

    Updat rabbitmq installation

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -2,12 +2,11 @@
 
 echo ">>> Installing RabbitMQ"
 
-apt-get -y install erlang-nox
+sudo apt-get -y install erlang-nox
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
-echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
-apt-get update
-apt-get install rabbitmq-server
+sudo apt-key add rabbitmq-signing-key-public.asc
+sudo apt-get update
+sudo apt-get install rabbitmq-server
 
-rabbitmqctl add_user $1 $2
-rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
+sudo rabbitmqctl add_user $1 $2
+sudo rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
