7185d8b8db950cf5e90f709889c71ed1dbf1d643
Thorsten Scheckenbach <t.scheckenbach@creativation.de>
2019-12-04 14:56:23 +0100

    Fix software-properties-common installation

--- scripts/base.sh
+++ scripts/base.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 
 # Install base packages
 # -qq implies -y --force-yes
-sudo apt-get install -qq curl unzip git-core ack-grep software-properties-common build-essential cachefilesd
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq curl unzip git-core ack-grep software-properties-common build-essential cachefilesd
 
 
 echo ">>> Installing *.xip.io self-signed SSL"
